759d7433d4ac3195263f4f6f296dc47e2b8a6534
cclu <Lkrnchocolateaaron@gmail.com>
2018-12-05 15:48:04 +0800

    changes

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 import Vue from 'vue'
 import App from './App'
-import store from './store/store'
+import Store from './store/store'
+import HeaderNav from './components/header/header.vue'
 
 import api from './util/index'
 
-Vue.prototype.$store = store
+Vue.prototype.$store = Store
 
 Vue.prototype.$api = api
 Vue.config.productionTip = false
 
+Vue.component('header-nav', HeaderNav)
 App.mpType = 'app'
 
 const app = new Vue({
-	store,
+	Store,
     ...App
 })
 app.$mount()
