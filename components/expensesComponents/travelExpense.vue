c11a919d8ba3d5f9d81d1155366159efad8aae31
杨柳 <yliung@163.com>
2018-12-06 17:39:11 +0800

    travel

---
+++ components/expensesComponents/travelExpense.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		111111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- pages/detail/expenseDetail.vue
+++ pages/detail/expenseDetail.vue
@@ -1,20 +1,33 @@
 <template>
-	<view class="travel-expense-detail">
+	<view class="expense-detail">
 		<header-nav />
 		<view class="page-body">
-			
+			<template v-if="expenseData.ACCEPT_TYPE==='401202'">
+				<travel-expense></travel-expense>
+			</template>
 		</view>
 	</view>
 </template>
 
 <script>
+	import travelExpense from '../../components/expensesComponents/travelExpense.vue'
 	export default {
 		data() {
 			return {
-				
+				expenseData:{} // 获取expenses页面参数
 			};
 		},
+		methods:{
+			// 获取页面数据
+			getExpenseData(){
+				
+			}
+		},
+		components:{
+			travelExpense
+		},
 		onLoad(option) {
+			this.expenseData = option
 			console.log(option)
 		}
 	}
