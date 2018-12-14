32bb433ac38e7b40468c1a668e5fe048a1661054
杨柳 <yliung@163.com>
2018-12-14 22:32:41 +0800

    移交

--- util/apiPath.js
+++ util/apiPath.js
@@ -4,7 +4,7 @@ export default {
     // baseURL: 'http://192.168.10.110:8010',
     // baseURL: 'http://60.191.61.27:8010',
     // baseURL: 'http://183.129.232.107:8010', // 正式环境ip
-    baseURL: 'http://192.168.10.123:8010', // 小平的
+    baseURL: 'http://192.168.11.193:8010', // 小平的
     // baseURL: 'http://cloud.eshangtech.com:6060',
     baseApiName: '/MobileServicePlatform/Handler/handler_ajax.ashx', // 接口
     
